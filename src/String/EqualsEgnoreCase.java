093acc3bda9ed400b9541c9359400ca3fd43608f
skwasim777 <wasimsk6061@gmail.com>
2023-10-24 22:38:16 +0530

    equalsegnorecase method in string

---
+++ src/String/EqualsEgnoreCase.java
@@ -0,0 +1,9 @@
+package String;
+
+public class EqualsEgnoreCase {
+		public static void main(String[] args) {
+			String s1 = "wasim";
+			String s2 ="WASIM";
+			System.out.println(s1.equalsIgnoreCase(s2));
+		}
+}
