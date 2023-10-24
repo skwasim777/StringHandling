a58243744a082bd9b9f29cae208a3c47750d130d
skwasim777 <wasimsk6061@gmail.com>
2023-10-24 22:23:42 +0530

    example of immutable string

---
+++ src/String/Example02.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Example02 {
+	public static void main(String[] args) {
+		String s = "Wasim";
+		s.concat("Shaikh");
+		// here output is wasim cause string is immutable
+		System.out.println(s);
+	}
+}
