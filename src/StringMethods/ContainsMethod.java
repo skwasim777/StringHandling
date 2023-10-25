67b066734b275d978d220de97dc3adbd7c489653
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 19:21:30 +0530

    contains method in string

---
+++ src/StringMethods/ContainsMethod.java
@@ -0,0 +1,12 @@
+package StringMethods;
+
+public class ContainsMethod {
+		public static void main(String[] args) {
+			String str = "Wasim";
+			System.out.println(str.contains("Wa"));
+			System.out.println(str.contains("Shaikh"));
+			String s = "what do u know about me";
+			System.out.println(s.contains("do u know"));
+			System.out.println(s.contains("Hello"));
+		}
+}
