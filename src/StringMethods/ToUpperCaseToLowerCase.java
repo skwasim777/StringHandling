50a8d0b9181785b19acc216f8d451cb22f5e5058
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 12:51:20 +0530

    toupercase and tolowercase method

---
+++ src/StringMethods/ToUpperCaseToLowerCase.java
@@ -0,0 +1,9 @@
+package StringMethods;
+
+public class ToUpperCaseToLowerCase {
+	public static void main(String[] args) {
+		String s = "Wasim";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+	}
+}
