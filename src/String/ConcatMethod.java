60ee3ee7e498fad8c73bbca7f87a204cd6747258
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 11:57:41 +0530

    concate methdo in string

---
+++ src/String/ConcatMethod.java
@@ -0,0 +1,10 @@
+package String;
+
+public class ConcatMethod {
+		public static void main(String[] args) {
+			String s1 = "wasim";
+			String s2 = "Shaikh";
+			String s3  = s1.concat(s2);
+			System.out.println(s3);
+		}
+}
