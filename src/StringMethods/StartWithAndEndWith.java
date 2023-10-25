fd8ded5f0ef77e475207b64fb8fdc7ee3e9d0479
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 16:00:49 +0530

    startwith and endwith method in string

---
+++ src/StringMethods/StartWithAndEndWith.java
@@ -0,0 +1,9 @@
+package StringMethods;
+
+public class StartWithAndEndWith {
+	public static void main(String[] args) {
+		String str = "Wasim";
+		System.out.println(str.startsWith("W"));
+		System.out.println(str.endsWith("m"));
+	}
+}
