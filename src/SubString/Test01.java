1f6c7203d574611ca519aa2fda82efdafee0d1e5
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 12:18:32 +0530

    substring method in string

---
+++ src/SubString/Test01.java
@@ -0,0 +1,10 @@
+package SubString;
+
+public class Test01 {
+	public static void main(String[] args) {
+		String s1 = "SachinTendulkar";
+		System.out.println("Original String " + s1);
+		System.out.println("SubString Starting from index 6 =" + s1.substring(6));
+		System.out.println("SubString Starting from index 0 to 6 =" + s1.substring(0, 6));
+	}
+}
