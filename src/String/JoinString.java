f754476f7daf854e90b1eb91fafa983d21fad0de
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 12:08:57 +0530

    join string

---
+++ src/String/JoinString.java
@@ -0,0 +1,10 @@
+package String;
+
+public class JoinString {
+		public static void main(String[] args) {
+			String s1 = "Wasim";
+			String s2 = "Shaikh";
+			String s3 = s1.join("",s1,s2);
+			System.out.println(s3);
+		}
+}
