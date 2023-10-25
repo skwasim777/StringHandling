f1c60104777f2b895e3cb1108dcb51b2796a9073
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 16:04:06 +0530

    charAt method in string

---
+++ src/StringMethods/CharAtMethod.java
@@ -0,0 +1,9 @@
+package StringMethods;
+
+public class CharAtMethod {
+	public static void main(String[] args) {
+		String str = "Wasim";
+		System.out.println(str.charAt(0));
+		System.out.println(str.charAt(4));
+	}
+}
