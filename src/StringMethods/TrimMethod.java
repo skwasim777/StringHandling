01010a2987eaf724f0144682ca22be6edca59bc8
skwasim777 <wasimsk6061@gmail.com>
2023-10-25 15:58:46 +0530

    trim method in string

---
+++ src/StringMethods/TrimMethod.java
@@ -0,0 +1,9 @@
+package StringMethods;
+
+public class TrimMethod {
+	public static void main(String[] args) {
+		String str = "  Wasim  ";
+		System.out.println(str);
+		System.out.println(str.trim());
+	}
+}
